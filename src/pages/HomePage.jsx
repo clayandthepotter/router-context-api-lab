0e85e3f018404675a9237ab3fe68328b3b5b82d4
Clayton Prickett <clayton.a.prickett@gmail.com>
2023-11-07 18:20:51 -0600

    update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import SearchBar from '../components/SearchBar';
+import SearchPage from './SearchPage';
 
 function HomePage() {
 	return (
 		<div className='home-page'>
 			<h1>Welcome to the Movie Search App</h1>
 			<p>Search for your favorite movies by name or keywords.</p>
-			<SearchBar />
+			<SearchPage/>
 		</div>
 	);
 }
